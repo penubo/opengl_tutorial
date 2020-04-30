b5d519678f76b260cbce81f7000bd73318fb3dd0
yongkim <penubokim@gmail.com>
2020-04-30 19:52:39 +0900

    Initial Commit

---
+++ opengl_tut/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  opengl_tut
+//
+//  Created by YongJoon Kim on 2020/04/30.
+//  Copyright © 2020 YongJoon Kim. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
