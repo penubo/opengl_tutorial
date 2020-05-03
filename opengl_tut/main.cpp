8475424af7a72704014f12a04bc74bf27a8b986e
yongkim <penubokim@gmail.com>
2020-05-03 13:01:53 +0900

    Bitmap example
    Issues:
    Viewport ratio is 2:1 by default so My raster distorted.
    I tried to fix it using gluPerspective but didn't work.
    How to map raster 1:1 to pixel viewport?

--- opengl_tut/main.cpp
+++ opengl_tut/main.cpp
@@ -22,32 +22,49 @@
 #include <iostream>
 #include <math.h>
 
+GLubyte rasters[24] = {
+    0xc0, 0x00,
+    0xc0, 0x00,
+    0xc0, 0x00,
+    0xc0, 0x00,
+    0xc0, 0x00,
+    0xff, 0x00,
+    0xff, 0x00,
+    0xc0, 0x00,
+    0xc0, 0x00,
+    0xc0, 0x00,
+    0xff, 0xc0,
+    0xff, 0xc0};
+
+
 void MyDisplay() {
     
-    
+
     glClear(GL_COLOR_BUFFER_BIT);
     glColor3f(0.5, 0.4, 0.3);
-    glBegin(GL_LINE_STRIP);
-    
-    glVertex3f(1.0, 1.0, 0.0);
-    glVertex3f(-1.0, 1.0, 0.0);
-    
-//    glVertex3f(-1.0, 1.0, 0.0);
-    glVertex3f(-1.0, -1.0, 0.0);
-    
-//    glVertex3f(-1.0, -1.0, 0.0);
-    glVertex3f(1.0, -1.0, 0.0);
-    
-//    glVertex3f(1.0, -1.0, 0.0);
-    glVertex3f(1.0, 1.0, 0.0);
 
-    
-    glEnd();
-    
-    
-    
+
+//    glRasterPos2i (100, 100);
+    glRasterPos2i(0, 0);
+    glBitmap (0, 6, 0.0, 0.0, 11.0, 0.0, rasters);
+    glBitmap (1, 6, 0.0, 0.0, 11.0, 0.0, rasters);
+
+    glBitmap (10, 12, 0.0, 0.0, 11.0, 0.0, rasters);
+    glBitmap (10, 12, 0.0, 0.0, 11.0, 0.0, rasters);
     glutSwapBuffers();
     glFlush();
+
+}
+
+void reshape(int w, int h)
+{
+    glViewport(0, 0, (GLsizei) w, (GLsizei) h);
+    glMatrixMode(GL_PROJECTION);
+    glLoadIdentity();
+//    gluPerspective(0, (GLdouble)h / (GLdouble)w, 0.01f, 100.0f);
+//    gluPerspective(0.0, 1440.0/900.0*float(w)/float(h), 0.1, 100.0);
+//    gluOrtho2D(-1 * w/2, w/2, -1 * h/2, h/2);
+    glMatrixMode(GL_MODELVIEW);
 
 }
 
@@ -55,11 +72,12 @@ int main(int argc, char * argv[]) {
     // insert code here...
     glutInit(&argc, argv);
     glutInitDisplayMode(GLUT_RGBA);
-    glutInitWindowSize(500, 500);
+
+    glutInitWindowSize(400, 300);
     glutInitWindowPosition(100, 100);
     glutCreateWindow("GLUT Sample");
-    glClearColor(1.0, 1.0, 1.0, 1.0);
-    glOrtho(-2.0, 2.0, -2.0, 2.0, -2.0, 2.0);
+
+    glutReshapeFunc(reshape);
     glutDisplayFunc(MyDisplay);
     glutMainLoop();
     return 0;
