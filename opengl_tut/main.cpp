01c584222f712762b7e8acd1e71388f655542421
yongkim <penubokim@gmail.com>
2020-05-03 10:59:43 +0900

    line strip example
    GL_LINE_STRIP needs n-1 vertexes to draw it

--- opengl_tut/main.cpp
+++ opengl_tut/main.cpp
@@ -27,18 +27,18 @@ void MyDisplay() {
     
     glClear(GL_COLOR_BUFFER_BIT);
     glColor3f(0.5, 0.4, 0.3);
-    glBegin(GL_LINES);
+    glBegin(GL_LINE_STRIP);
     
     glVertex3f(1.0, 1.0, 0.0);
     glVertex3f(-1.0, 1.0, 0.0);
     
-    glVertex3f(-1.0, 1.0, 0.0);
+//    glVertex3f(-1.0, 1.0, 0.0);
     glVertex3f(-1.0, -1.0, 0.0);
     
-    glVertex3f(-1.0, -1.0, 0.0);
+//    glVertex3f(-1.0, -1.0, 0.0);
     glVertex3f(1.0, -1.0, 0.0);
     
-    glVertex3f(1.0, -1.0, 0.0);
+//    glVertex3f(1.0, -1.0, 0.0);
     glVertex3f(1.0, 1.0, 0.0);
 
     
